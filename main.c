8263f7be9a809ea2692e9e43e7ab6269d10c4977
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-10-27 00:49:12 -0700

    General function using execve

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main() {
-    printf("Hello, World!\n");
+
+int main(int argc, char* argv[]) {
+    char** newargv = (char**)malloc(sizeof(char*) * argc);
+
+    for(int iter = 1; iter < argc; iter++) {
+      newargv[iter - 1] = (char*)argv[iter];
+    }
+    newargv[argc - 1] = NULL;
+
+    int child_pid = fork();
+    if(child_pid == 0) {
+      execve(newargv[0], newargv, NULL);
+      printf("Error: execve failed\n");
+      _exit(1);
+    }
+
+    printf("%s pid is %d. forked %d. "
+           "Parent exiting\n", argv[0], getpid(), child_pid);
     return 0;
 }
