5afdd4f9d70185a1c8f891772904dc091dc54f4f
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-10-28 22:25:21 -0700

    Resolved main code to exec cmd with arguements

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 #define PATH "/usr/bin/"
 
@@ -14,66 +15,62 @@
 
 
 int main(int argc, char* argv[]) {
-    int fds[2];
-    pipe(fds);
-    pid_t pid = fork();
-
-    if (pid == 0) {
-      dup2(fds[1], STDIN_FILENO);
-      close(fds[0]);
-      close(fds[1]);
-      if(execve(argv[0], argv, NULL) == -1) { exit(0); }
+    if(argc < 4) {
+      fprintf(stderr, "Usage: %s <inp> <cmd> <out>\n", argv[0]);
+      return 1;
     }
-
-    close(fds[0]);
-    const char* input[] = {"echo", "-l", "tree", NULL};
-    for(int i = 0; input[i] != NULL; i++) {
-      dprintf(fds[1], "%s\n", input[i]);
-    }
-
-    close(fds[1]);
-    int status;
-    pid_t wpid = waitpid(pid, &status, 0);
-    return wpid == pid && WIFEXITED(status) ? WEXITSTATUS(status) : -1;
-    // if(argc != 4) {
-    //   fprintf(stderr, "Usage: %s <inp> <cmd> <out>\n", argv[0]);
-    //   return 1;
-    // }
     
-    // int inputFD = open(argv[1], O_RDONLY, S_IRUSR);
-    // if(inputFD == -1) {
-    //   fprintf(stderr, "Failed to open %s\n", argv[1]);
-    //   return 1;
-    // }
+    int inputFD;
+    if(strcmp(argv[1], "-") == 0) {
+      printf("default input\n");
+    }
+    else {
+      inputFD = open(argv[1], O_RDONLY, S_IRUSR);
+      if(inputFD == -1) {
+        fprintf(stderr, "Failed to open %s\n", argv[1]);
+        return 1;
+      }
+    }
 
-    // int outputFD = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
-    // if(outputFD == -1) {
-    //   fprintf(stderr, "Failed to open %s\n", argv[argc - 1]);
-    //   return 1;
-    // }
+    int outputFD;
+    if(strcmp(argv[3], "-") == 0) {
+      printf("default output\n");
+    }
+    else {
+      outputFD = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
+      if(outputFD == -1) {
+        fprintf(stderr, "Failed to open %s\n", argv[argc - 1]);
+        return 1;
+      }
+    }
 
-    // // Need to read the input file and write each command as separated parameter
-    // // to the newargv array
-    // char** newargv = (char**)malloc(sizeof(char*) * (argc - 2)); // change to number of elements + cmd selected
+    // Need to read the input file and write each command as separated parameter
+    // to the newargv array
+    char** newargv = (char**)malloc(sizeof(char*) * (argc - 2)); // change to number of elements + cmd selected
 
 
     // // REMOVE
-    // for(int iter = 2; iter < argc; iter++) {
-    //   newargv[iter - 2] = (char*)argv[iter];
-    // }
-    // newargv[argc - 2 - 1] = NULL;
+    for(int iter = 2; iter < argc - 1; iter++) {
+      newargv[iter - 2] = (char*)argv[iter];
+    }
+    newargv[argc - 3] = NULL;
 
-    
 
-    // // Fork and execute
-    // int child_pid = fork();
-    // if(child_pid == 0) {
-    //   execve(newargv[0], newargv, NULL);
-    //   printf("Error: execve failed\n");
-    //   _exit(1);
-    // }
+    // Fork and execute
+    int child_pid = fork();
+    if(child_pid == 0) {
+      dup2(inputFD, STDIN_FILENO);
+      close(inputFD);
+      dup2(outputFD, STDOUT_FILENO);
+      close(outputFD);
 
-    // printf("%s pid is %d. forked %d. "
-    //        "Parent exiting\n", argv[0], getpid(), child_pid);
-    // return 0;
+      execve(newargv[0], newargv, NULL);
+      printf("Error: execve failed\n");
+      _exit(1);
+    }
+
+
+    printf("%s pid is %d. forked %d. "
+           "Parent exiting\n", argv[0], getpid(), child_pid);
+    return 0;
 }
